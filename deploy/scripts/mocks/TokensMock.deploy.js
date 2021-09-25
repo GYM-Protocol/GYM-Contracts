1a70023812cdd2f05fb89ee4bf36a28f5abb927b
gor1994 <karapetyan.gor94@gmail.com>
2021-09-25 12:35:36 +0400

    add tokens mock deployment task

--- deploy/scripts/mocks/TokensMock.deploy.js
+++ deploy/scripts/mocks/TokensMock.deploy.js
@@ -1,53 +1,49 @@
-const { ethers } = require("ethers");
-const { contractDeploy } = require("../../utils");
-const { VARIABLES } = require("../../../utils");
+const { VARIABLES } = require("../../../utils/constants");
 
-module.exports = async function (hre) {
-    const chainId = await getChainId();
-    if (chainId === "31337") {
-        let options1 = {
-            contractName: "WantToken1",
-            contractFactory: "ERC20Mock",
-            args: ["WantToken1", "WT1", ethers.utils.parseEther("100000000")],
-        };
+module.exports = async function ({ getChainId, run, ethers }) {
+	const chainId = await getChainId();
+	if (chainId === "31337") {
+		await run("deploy:tokensMock", {
+			contractName: "WantToken1",
+			symbol: "WT1",
+			supply: `${ethers.utils.parseEther("100000000")}`
+		});
 
-        let options2 = {
-            contractName: "WantToken2",
-            contractFactory: "ERC20Mock",
-            args: ["WantToken2", "WT2", VARIABLES.hardhat.TEST_WANTTOKEN2_AMOUNT],
-        };
+		await run("deploy:tokensMock", {
+			contractName: "WantToken2",
+			symbol: "WT2",
+			supply: `${VARIABLES.hardhat.TEST_WANTTOKEN2_AMOUNT}`
+		});
 
-        let options3 = {
-            contractName: "EarnToken",
-            contractFactory: "ERC20Mock",
-            args: ["EarnToken", "ET", VARIABLES.hardhat.TEST_TOKENS_MINT_AMOUNT],
-        };
+		await run("deploy:tokensMock", {
+			contractName: "EarnToken",
+			symbol: "ET",
+			supply: `${VARIABLES.hardhat.TEST_TOKENS_MINT_AMOUNT}`
+		});
 
-        let options4 = {
-            contractName: "TokenA",
-            contractFactory: "ERC20Mock",
-            args: ["TokenA", "TA", VARIABLES.hardhat.TEST_TOKENS_MINT_AMOUNT],
-        };
+		// const options4 = {
+		// 	contractName: "TokenA",
+		// 	contractFactory: "ERC20Mock",
+		// 	args: ["TokenA", "TA", VARIABLES.hardhat.TEST_TOKENS_MINT_AMOUNT]
+		// };
+		await run("deploy:tokensMock", {
+			contractName: "TokenA",
+			symbol: "TA",
+			supply: `${VARIABLES.hardhat.TEST_TOKENS_MINT_AMOUNT}`
+		});
 
-        let options5 = {
-            contractName: "TokenB",
-            contractFactory: "ERC20Mock",
-            args: ["TokenB", "TB", VARIABLES.hardhat.TEST_TOKENS_MINT_AMOUNT],
-        };
+		await run("deploy:tokensMock", {
+			contractName: "TokenB",
+			symbol: "TB",
+			supply: `${VARIABLES.hardhat.TEST_TOKENS_MINT_AMOUNT}`
+		});
 
-        let options6 = {
-            contractName: "ibToken",
-            contractFactory: "ERC20Mock",
-            args: ["ibToken", "IT", VARIABLES.hardhat.TEST_TOKENS_MINT_AMOUNT],
-        };
-
-        await contractDeploy(hre, options1);
-        await contractDeploy(hre, options2);
-        await contractDeploy(hre, options3);
-        await contractDeploy(hre, options4);
-        await contractDeploy(hre, options5);
-        await contractDeploy(hre, options6);
-    }
+		await run("deploy:tokensMock", {
+			contractName: "ibToken",
+			symbol: "IT",
+			supply: `${VARIABLES.hardhat.TEST_TOKENS_MINT_AMOUNT}`
+		});
+	}
 };
 
 module.exports.tags = ["Tokens"];
--- tasks/deploy/mocks/index.js
+++ tasks/deploy/mocks/index.js
@@ -4,7 +4,21 @@ task("deploy:strategyMock", "strategyMock deploy", require("./strategy.deploy"))
 	.addParam("contractName", "name Of contract")
 	.addParam("wantAddress", "strategy want token address");
 
+task("deploy:tokensMock", "tokensMock deploy", require("./tokensMock.deploy"))
+	.addParam("contractName", "name Of contract")
+	.addParam("symbol", "symbol")
+	.addParam("supply", "supply");
+
+task("deploy:farmMock", "farmMock deploy", require("./farmMock.deploy"))
+	.addParam("wantAddress", "want token address")
+	.addParam("earnAddress", "earn token address");
+
 task("deploy:config", "Config deploy", require("./config.deploy")).addParam("wbnbAddress", "WBNB address");
+
+task("deploy:routerMock", "RouterMock deploy", require("./routerMock.deploy")).addParam(
+	"wantAddress",
+	"want token address"
+);
 
 task("deploy:bankMock", "bankMock deploy", require("./bankMock.deploy"));
 
