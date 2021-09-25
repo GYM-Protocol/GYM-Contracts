8be13863f7c200df671f5c8e4703757ce2175c30
gor1994 <karapetyan.gor94@gmail.com>
2021-09-25 11:24:34 +0400

    add bankMock deployment task

--- tasks/deploy/mocks/index.js
+++ tasks/deploy/mocks/index.js
@@ -3,3 +3,5 @@ const { task } = require("hardhat/config");
 task("deploy:strategyMock", "strategyMock deploy", require("./strategy.deploy"))
 	.addParam("contractName", "name Of contract")
 	.addParam("wantAddress", "strategy want token address");
+
+task("deploy:bankMock", "bankMock deploy", require("./bankMock.deploy"));
