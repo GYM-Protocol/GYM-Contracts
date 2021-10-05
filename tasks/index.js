8a40196843d3ba03a99e43ac3915b780b005fe1b
gor1994 <karapetyan.gor94@gmail.com>
2021-10-05 18:11:56 +0400

    fix index file in tasks

--- tasks/index.js
+++ tasks/index.js
@@ -1,162 +1,140 @@
+const { task } = require("hardhat/config");
 require("./utils");
 require("./deploy");
 require("./gymVaultsBank");
 require("./gymMLM");
 
+task("setupNext", "Add liquidity without args").setAction(
+	async (
+		taskArgs,
+		{
+			config,
+			ethers: {
+				getNamedSigners,
+				getContractAt,
+				getContract,
+				utils: { parseEther }
+			}
+		}
+	) => {
+		const variables = config.solpp.defs;
+		const accounts = await getNamedSigners();
+		const routerAddress = variables.ROUTER;
+		const router = await getContractAt("IPancakeRouter02", routerAddress);
+		const gymToken = await getContract("GymToken", accounts.deployer);
+		await gymToken.connect(accounts.holder).approve(routerAddress, parseEther("1000"));
+		const tx = await router
+			.connect(accounts.holder)
+			.addLiquidityETH(
+				gymToken.address,
+				parseEther("1000"),
+				0,
+				0,
+				accounts.holder.address,
+				new Date().getTime() + 20,
+				{
+					value: parseEther("0.1"),
+					gasLimit: 5000000
+				}
+			);
+		console.log("🚀 ~ file: index.js ~ line 58 ~ .setAction ~ tx", tx);
 
-// task("addPool", "Add new pool to bank")
-// 	.addParam("bank", "Bank address")
-// 	.addParam("want", "Want token address")
-// 	.addParam("allocPoint", "Pool allocation point")
-// 	.addParam("withUpdate", "Will be pool update")
-// 	.addParam("strategy", "Strategy address")
-// 	.setAction(async (taskArgs) => {
-// 		const accounts = await ethers.getNamedSigners();
-// 		const gymVaultsBank = await ethers.getContractAt("GymVaultsBank", taskArgs.bank, accounts.deployer);
+		const factory = await getContractAt("IPancakeFactory", await router.factory());
+		const pairAddress = await factory.getPair("0xDfb1211E2694193df5765d54350e1145FD2404A1", gymToken.address);
+		console.log("🚀 ~ file: index.js ~ line 66 ~ .setAction ~ pairAddress", pairAddress);
+		console.log("Succeeded");
+	}
+);
 
-// 		await gymVaultsBank.add(taskArgs.want, taskArgs.allocPoint, taskArgs.withUpdate, taskArgs.strategy);
+task("setup", "Contracts Setup").setAction(
+	async (
+		taskArgs,
+		{
+			ethers: {
+				getNamedSigners,
+				getContractAt,
+				getContract,
+				utils: { parseEther }
+			},
+			config
+		}
+	) => {
+		const variables = config.solpp.defs;
+		console.log("🚀 ~ file: index.js ~ line 91 ~ variables", variables);
 
-// 		console.log("Pool added");
-// 	});
+		const accounts = await getNamedSigners();
+		const WBNB = variables.WBNB_TOKEN;
+		const BUSD = variables.BUSD;
+		const routerAddress = variables.ROUTER;
 
-// task("addLiquidity", "Add liquidity")
-// 	.addParam("router", "PanCakeRouter address")
-// 	.addParam("token", "Token address")
-// 	.addParam("tokenAmount", "Amount of token to add liquidity")
-// 	.addParam("bnbAmount", "Amount of bnb to add liquidity")
-// 	.addParam("to", "LP tokens receiver")
-// 	.setAction(async (taskArgs) => {
-// 		const accounts = await ethers.getNamedSigners();
-// 		const router = await ethers.getContractAt("IPancakeRouter02", taskArgs.router);
-// 		const token = await ethers.getContractAt("GymToken", taskArgs.token);
-// 		await token.connect(accounts.holder).approve(taskArgs.router, ethers.utils.parseEther(taskArgs.tokenAmount));
-// 		const tx = await router
-// 			.connect(accounts.holder)
-// 			.addLiquidityETH(
-// 				taskArgs.token,
-// 				ethers.utils.parseEther(taskArgs.tokenAmount),
-// 				0,
-// 				0,
-// 				taskArgs.to,
-// 				new Date().getTime() + 20,
-// 				{
-// 					value: ethers.utils.parseEther(taskArgs.bnbAmount)
-// 				}
-// 			);
+		const router = await getContractAt("IPancakeRouter02", routerAddress);
+		const factory = await getContractAt("IPancakeFactory", await router.factory());
+		const gymToken = await getContract("GymToken");
+		const busdToken = await getContractAt("GymToken", BUSD);
+		const bank = await getContract("GymVaultsBank");
+		const buyBack = await getContract("BuyBack");
+		const farming = await getContract("GymFarming");
+		const strategy = await getContract("GymVaultsStrategyAlpaca");
+		const strategyBUSD = await getContract("GymVaultsStrategyAlpacaBUSD");
+		const gymMLM = await getContract("GymMLM");
+		await gymToken.connect(accounts.holder).approve(routerAddress, parseEther("2000000000"));
+		await busdToken.connect(accounts.holder).approve(routerAddress, parseEther("100000000"));
+		await busdToken.connect(accounts.holder).approve(bank.address, parseEther("1000000000"));
+		await gymToken.connect(accounts.holder).delegate(accounts.holder.address, {
+			gasLimit: 5000000
+		});
+		await router
+			.connect(accounts.holder)
+			.addLiquidity(
+				gymToken.address,
+				BUSD,
+				parseEther("1000"),
+				parseEther("1000"),
+				0,
+				0,
+				accounts.holder.address,
+				new Date().getTime() + 20,
+				{
+					gasLimit: 5000000
+				}
+			);
 
-// 		console.log("🚀 ~ file: index.js ~ line 169 ~ .setAction ~ tx", tx.hash);
-// 	});
+		await router
+			.connect(accounts.holder)
+			.addLiquidityETH(
+				gymToken.address,
+				parseEther("1000"),
+				0,
+				0,
+				accounts.holder.address,
+				new Date().getTime() + 20,
+				{
+					value: parseEther("0.1"),
+					gasLimit: 5000000
+				}
+			);
+		console.log("Succeeded");
 
-// task("setupNext", "Add liquidity without args").setAction(async () => {
-// 	const accounts = await ethers.getNamedSigners();
-// 	const routerAddress = "0x367633909278A3C91f4cB130D8e56382F00D1071";
-// 	const router = await ethers.getContractAt("IPancakeRouter02", routerAddress);
-// 	const gymToken = await ethers.getContract("GymToken", accounts.deployer);
-// 	await gymToken.connect(accounts.holder).approve(routerAddress, ethers.utils.parseEther("1000"));
-// 	const tx = await router
-// 		.connect(accounts.holder)
-// 		.addLiquidityETH(
-// 			gymToken.address,
-// 			ethers.utils.parseEther("1000"),
-// 			0,
-// 			0,
-// 			accounts.holder.address,
-// 			new Date().getTime() + 20,
-// 			{
-// 				value: ethers.utils.parseEther("0.1"),
-// 				gasLimit: 5000000
-// 			}
-// 		);
-// 	console.log("🚀 ~ file: index.js ~ line 58 ~ .setAction ~ tx", tx);
+		await bank.connect(accounts.deployer).setTreasuryAddress(accounts.owner.address);
+		await bank.connect(accounts.deployer).setFarmingAddress(farming.address);
+		await bank.connect(accounts.deployer).setWithdrawFee(1000);
+		await gymMLM.connect(accounts.deployer).setBankAddress(bank.address);
 
-// 	const factory = await ethers.getContractAt("IPancakeFactory", await router.factory());
-// 	const pairAddress = await factory.getPair("0xDfb1211E2694193df5765d54350e1145FD2404A1", gymToken.address);
-// 	console.log("🚀 ~ file: index.js ~ line 66 ~ .setAction ~ pairAddress", pairAddress);
-// 	console.log("Succeeded");
-// });
+		const pairAddress = await factory.getPair(WBNB, gymToken.address);
+		await farming.connect(accounts.deployer).add(30, pairAddress, false, {
+			gasLimit: 5000000
+		});
 
-// task("setup", "Contracts Setup").setAction(async () => {
-// 	const NETWORK = process.env.NETWORK || "hardhat";
-// 	const variables = VARIABLES[`${NETWORK}`];
+		await gymToken.connect(accounts.holder).transfer(bank.address, parseEther("3000000"));
+		await gymToken.connect(accounts.holder).transfer(farming.address, parseEther("2000000"));
 
-// 	const accounts = await ethers.getNamedSigners();
-// 	const WBNB = variables.contracts[2];
-// 	const BUSD = variables.contracts[5];
-// 	const routerAddress = variables.contracts[0];
+		await bank.connect(accounts.deployer).add(WBNB, 30, false, strategy.address, {
+			gasLimit: 5000000
+		});
+		await bank.connect(accounts.deployer).add(BUSD, 30, false, strategyBUSD.address, {
+			gasLimit: 5000000
+		});
 
-// 	const router = await ethers.getContractAt("IPancakeRouter02", routerAddress);
-// 	const factory = await ethers.getContractAt("IPancakeFactory", await router.factory());
-// 	const gymToken = await ethers.getContract("GymToken");
-// 	const busdToken = await ethers.getContractAt("GymToken", BUSD);
-// 	const bank = await ethers.getContract("GymVaultsBank");
-// 	const buyBack = await ethers.getContract("BuyBack");
-// 	const farming = await ethers.getContract("GymFarming");
-// 	const strategy = await ethers.getContract("GymVaultsStrategyAlpaca");
-// 	const strategyBUSD = await ethers.getContract("GymVaultsStrategyAlpacaBUSD");
-// 	const gymMLM = await ethers.getContract("GymMLM");
-// 	await gymToken.connect(accounts.holder).approve(routerAddress, ethers.utils.parseEther("2000000000"));
-// 	await busdToken.connect(accounts.holder).approve(routerAddress, ethers.utils.parseEther("100000000"));
-// 	await busdToken.connect(accounts.holder).approve(bank.address, ethers.utils.parseEther("1000000000"));
-// 	await gymToken.connect(accounts.holder).delegate(accounts.holder.address, {
-// 		gasLimit: 5000000
-// 	});
-// 	await router
-// 		.connect(accounts.holder)
-// 		.addLiquidity(
-// 			gymToken.address,
-// 			BUSD,
-// 			ethers.utils.parseEther("1000"),
-// 			ethers.utils.parseEther("1000"),
-// 			0,
-// 			0,
-// 			accounts.holder.address,
-// 			new Date().getTime() + 20,
-// 			{
-// 				gasLimit: 5000000
-// 			}
-// 		);
-
-// 	await router
-// 		.connect(accounts.holder)
-// 		.addLiquidityETH(
-// 			gymToken.address,
-// 			ethers.utils.parseEther("1000"),
-// 			0,
-// 			0,
-// 			accounts.holder.address,
-// 			new Date().getTime() + 20,
-// 			{
-// 				value: ethers.utils.parseEther("0.1"),
-// 				gasLimit: 5000000
-// 			}
-// 		);
-// 	console.log("Succeeded");
-
-// 	await bank.connect(accounts.deployer).setTreasuryAddress(accounts.owner.address);
-// 	await bank.connect(accounts.deployer).setFarmingAddress(farming.address);
-// 	await bank.connect(accounts.deployer).setWithdrawFee(1000);
-// 	await gymMLM.connect(accounts.deployer).setBankAddress(bank.address);
-
-// 	const pairAddress = await factory.getPair(WBNB, gymToken.address);
-// 	await farming.connect(accounts.deployer).add(30, pairAddress, false, {
-// 		gasLimit: 5000000
-// 	});
-
-// 	await gymToken.connect(accounts.holder).transfer(bank.address, ethers.utils.parseEther("3000000"));
-// 	await gymToken.connect(accounts.holder).transfer(farming.address, ethers.utils.parseEther("2000000"));
-
-// 	await bank.connect(accounts.deployer).add(WBNB, 30, false, strategy.address, {
-// 		gasLimit: 5000000
-// 	});
-// 	await bank.connect(accounts.deployer).add(BUSD, 30, false, strategyBUSD.address, {
-// 		gasLimit: 5000000
-// 	});
-
-// 	console.log("Succeeded");
-// });
-
-// task("deployMocks", "Deploying mocks for tests").setAction(async () => {
-// 	await hre.run("deploy", {
-// 		deployScripts: "deploy/scripts/mocks"
-// 	});
-// });
+		console.log("Succeeded");
+	}
+);
