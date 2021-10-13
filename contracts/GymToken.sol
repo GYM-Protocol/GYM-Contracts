a337876bd74bffeac30ec05258952b0fff333b3d
rootNad <gevork.simonyan@exio.tech>
2021-10-13 17:30:39 +0400

    Merge conflicts

--- contracts/GymToken.sol
+++ contracts/GymToken.sol
@@ -68,7 +68,6 @@ contract GymToken {
 
     constructor(address account) {
         balances[account] = uint96(totalSupply);
-		console.log("TRANSFER");
         emit Transfer(address(0), account, totalSupply);
     }
 
@@ -334,13 +333,6 @@ contract GymToken {
             checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, newVotes);
             numCheckpoints[delegatee] = nCheckpoints + 1;
         }
-		console.log("sol delegatee src is: ", delegatee);
-		console.log("sol oldVotes  src is: ", oldVotes);
-		console.log("sol newVotes  src is: ", newVotes);
-
-		console.log("sol delegatee dst is: ", delegatee);
-		console.log("sol oldVotes  dst is: ", oldVotes);
-		console.log("sol newVotes  dst is: ", newVotes);
         emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
     }
 
